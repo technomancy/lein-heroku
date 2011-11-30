6b57cf500bfebd244c218be8a6361f9fab51ddd3
Phil Hagelberg <technomancy@gmail.com>
2011-11-30 14:57:37 -0800

    Rework key removal to use remote listing.

--- src/leiningen/heroku/keys.clj
+++ src/leiningen/heroku/keys.clj
@@ -3,7 +3,7 @@
             [clojure.java.io :as io]
             [clojure.string :as string]))
 
-(defn- discover-keys []
+(defn- local-keys []
   (filter #(.endsWith % ".pub")
           (.list (io/file (System/getProperty "user.home") ".ssh"))))
 
@@ -11,6 +11,20 @@
   (if (.startsWith key "/")
     (slurp key)
     (slurp (io/file (System/getProperty "user.home") ".ssh" key))))
+
+(defn- remote-keys []
+  ;; TODO: will this be exposed on HerokuAPI object?
+  (-> (util/get-credentials)
+      (second)
+      (com.heroku.api.connection.HttpClientConnection.)
+      (.execute (com.heroku.api.request.key.KeyList.))
+      (.getData)))
+
+(defn- key-comment [key]
+  (let [contents (try (read-key key)
+                      (catch java.io.FileNotFoundException _
+                        key))]
+    (string/trim (last (.split contents " ")))))
 
 (defn keys:add
   "Add the given SSH key or keys to your account. If no keys are given,
@@ -24,37 +38,36 @@ will search in $HOME/.ssh and prompt for each."
        (catch Exception e
          (println (or (.getMessage e) e)))))
   ([]
-     (doseq [key (discover-keys)]
+     (doseq [key (local-keys)]
        (when (util/prompt-for (format "Found key %s. Add? [Y/n] " key))
          (keys:add key)))))
 
 (defn keys:remove
   "Remove the given SSH key or keys from your account. If no keys are given,
-will search in $HOME/.ssh and prompt for each."
+will list all uploaded keys and prompt for removal of each."
   ([key & keys]
      (doseq [key (cons key keys)]
        (keys:remove key)))
   ([key]
      (println (format "Removing %s." key))
      (try
-       (.removeKey (util/api) (string/trim (last (.split (read-key key) " "))))
+       (.removeKey (util/api) (key-comment key))
        (catch Exception e
          (println (or (.getMessage e) e)))))
   ([]
-     (when (util/prompt-for "Remove all keys? ")
-       ;; TODO: should really list keys from API, not locally.
-       (doseq [key (discover-keys)]
-         (keys:remove key)))))
+     (doseq [key (remote-keys)
+             :let [comment (key-comment (get key "contents"))]]
+       (when (util/prompt-for (format "Found key %s. Remove? [Y/n] " comment))
+         (keys:remove comment)))))
 
 (defn keys:list
-  "Display keys for the current user."
+  "Display all keys for the current user."
   []
-  ;; TODO: will this be exposed on HerokuAPI?
-  (let [[email key] (util/get-credentials)
-        connection (com.heroku.api.connection.HttpClientConnection. key)
-        response (.execute connection (com.heroku.api.request.key.KeyList.))]
-    (println "=== Keys for" email)
-    (doseq [key (.getData response)
+  (let [keys (remote-keys)
+        [email] (util/get-credentials)
+        keys-or-key (if (> (count keys) 1) "keys" "key")]
+    (println "===" (count keys) keys-or-key "for" email)
+    (doseq [key (remote-keys)
             :let [[type key comment] (.split (get key "contents") " ")
                   summary (str (apply str (take 10 key)) "..."
                                (apply str (drop (- (count key) 10) key)))]]
