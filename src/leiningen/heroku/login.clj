3e0559ad6b114f448da71d4c43f040a075e506a6
Phil Hagelberg <technomancy@gmail.com>
2011-11-30 11:55:53 -0800

    Fix login command with newly-exposed API key.

--- src/leiningen/heroku/login.clj
+++ src/leiningen/heroku/login.clj
@@ -1,7 +1,8 @@
 (ns leiningen.heroku.login
   (:require [leiningen.heroku.util :as util]
             [clojure.java.io :as io])
-  (:import (com.heroku.api.command.login BasicAuthLogin)))
+  (:import (com.heroku.api.request.login BasicAuthLogin)
+           (com.heroku.api.connection HttpClientConnection)))
 
 (defn- read-password []
   (.readPassword (System/console) "%s" (into-array ["Password: "])))
@@ -11,10 +12,10 @@
   (print "Email: ") (flush)
   (let [email (read-line)
         password (String. (read-password))
-        command (BasicAuthLogin. email password)
-        response nil ;; API key is currently not exposed by underlying Java API
-        api-key (.api_key response)]
+        connection (HttpClientConnection. (BasicAuthLogin. email password))
+        api-key (.getApiKey connection)]
     (with-open [w (io/writer (util/credentials-file))]
-      (.write email)
-      (.write "\n")
-      (.write api-key))))
+      (.write w email)
+      (.write w "\n")
+      (.write w api-key))
+    (println "Saved API key locally.")))
