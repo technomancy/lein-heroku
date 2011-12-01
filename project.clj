9fbd650d1062771acce3bdc5f3907158f3eeb1b0
Phil Hagelberg <technomancy@gmail.com>
2011-12-01 15:10:39 -0800

    Switch to org.clojars.technomancy snapshots for the time being.
    We can use the official ones once they land in sonatype snapshots.

--- project.clj
+++ project.clj
@@ -2,10 +2,11 @@
   :description "Heroku plugin for Leiningen"
   :dependencies [[lein-newnew "0.1.2"]
                  [org.clojure/tools.cli "0.2.1"]
-                 [com.heroku.api/heroku-api "0.1-SNAPSHOT"]
-                 [com.heroku.api/heroku-http-apache "0.1-SNAPSHOT"]
-                 [com.heroku.api/heroku-json-gson "0.1-SNAPSHOT"]
+                 [org.clojars.technomancy/heroku-api "0.1-20111019.122151-1"]
+                 [org.clojars.technomancy/heroku-http-apache "0.1-20111019.122151-1"]
+                 [org.clojars.technomancy/heroku-json-gson "0.1-20111019.122151-1"]
                  [doric "0.6.0"]]
   :eval-in-leiningen true
-  :repositories {"sontype-snapshots"
-                 "https://oss.sonatype.org/content/repositories/snapshots/"})
+  ;; :repositories {"sontype-snapshots"
+  ;;                "https://oss.sonatype.org/content/repositories/snapshots/"}
+  )
