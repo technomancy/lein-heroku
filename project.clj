1e75ace47bbe0049b6be8bf4f8904a7011a283b0
Phil Hagelberg <technomancy@gmail.com>
2012-04-06 14:50:22 -0700

    Version 0.1.1.

--- project.clj
+++ project.clj
@@ -1,8 +1,9 @@
-(defproject lein-heroku "0.1.0"
+(defproject lein-heroku "0.1.1"
   :description "Heroku plugin for Leiningen"
   :license {:name "Eclipse Public License"}
   :dependencies [[lein-newnew "0.1.2"]
-                 [org.clojure/tools.cli "0.2.1"]
+                 [org.clojure/tools.cli "0.2.1"
+                  :exclusions [org.clojure/clojure]]
                  [org.clojars.technomancy/heroku-api "0.1-20111019.122151-1"]
                  [org.clojars.technomancy/heroku-http-apache "0.1-20111019.122151-1"]
                  [org.clojars.technomancy/heroku-json-gson "0.1-20111019.122151-1"]
