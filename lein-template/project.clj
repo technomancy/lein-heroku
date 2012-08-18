588d2efa08f09633844a1ede2a3ea6e3a9b6d9c6
Phil Hagelberg <technomancy@gmail.com>
2012-08-17 17:34:02 -0700

    Release 0.2.0 of heroku/lein-template.

--- lein-template/project.clj
+++ lein-template/project.clj
@@ -1,4 +1,4 @@
-(defproject heroku/lein-template "0.2.0-SNAPSHOT"
+(defproject heroku/lein-template "0.2.0"
   :description "Template for new ring-based Leiningen projects for Heroku."
   :url "https://github.com/technomancy/lein-heroku"
   :license {:name "Eclipse Public License"
