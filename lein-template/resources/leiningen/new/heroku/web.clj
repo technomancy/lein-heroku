88c6fff1c318464bb9a286ead9a52ced8e4abb99
Phil Hagelberg <technomancy@gmail.com>
2014-05-13 09:17:53 -0700

    Move middleware wrapping out of -main.

--- lein-template/resources/leiningen/new/heroku/web.clj
+++ lein-template/resources/leiningen/new/heroku/web.clj
@@ -38,16 +38,18 @@
             :headers {"Content-Type" "text/html"}
             :body (slurp (io/resource "500.html"))}))))
 
+(defn wrap-app [app]
+  (-> app
+      ((if (env :production)
+         wrap-error-page
+         trace/wrap-stacktrace))
+      (site {:session {:store store}})))
+
 (defn -main [& [port]]
   (let [port (Integer. (or port (env :port) 5000))
         ;; TODO: heroku config:add SESSION_SECRET=$RANDOM_16_CHARS
         store (cookie/cookie-store {:key (env :session-secret)})]
-    (jetty/run-jetty (-> #'app
-                         ((if (env :production)
-                            wrap-error-page
-                            trace/wrap-stacktrace))
-                         (site {:session {:store store}}))
-                     {:port port :join? false})))
+    (jetty/run-jetty (wrap-app #'app) {:port port :join? false})))
 
 ;; For interactive development:
 ;; (.stop server)
