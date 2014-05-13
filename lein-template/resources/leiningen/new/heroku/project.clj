b251b76d2df168cfea40b3e1ae9488a9bbc79010
Phil Hagelberg <technomancy@gmail.com>
2014-05-13 09:18:02 -0700

    Bump version numbers for template.

--- lein-template/resources/leiningen/new/heroku/project.clj
+++ lein-template/resources/leiningen/new/heroku/project.clj
@@ -3,12 +3,12 @@
   :url "http://{{name}}.herokuapp.com"
   :license {:name "FIXME: choose"
             :url "http://example.com/FIXME"}
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [compojure "1.1.1"]
-                 [ring/ring-jetty-adapter "1.1.0"]
-                 [ring/ring-devel "1.1.0"]
-                 [ring-basic-authentication "1.0.1"]
-                 [environ "0.2.1"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [compojure "1.1.8"]
+                 [ring/ring-jetty-adapter "1.2.2"]
+                 [ring/ring-devel "1.2.2"]
+                 [ring-basic-authentication "1.0.5"]
+                 [environ "0.5.0"]
                  [com.cemerick/drawbridge "0.0.6"]]
   :min-lein-version "2.0.0"
   :plugins [[environ/environ.lein "0.2.1"]]
