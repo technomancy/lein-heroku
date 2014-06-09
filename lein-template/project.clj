82120b26d092eeb43ccbeba589788a5b644b747a
Phil Hagelberg <technomancy@gmail.com>
2014-06-09 15:42:57 -0700

    Add non-tagging release-tasks.

--- lein-template/project.clj
+++ lein-template/project.clj
@@ -2,4 +2,13 @@
   :description "Template for new Heroku Compojure web apps."
   :url "https://github.com/technomancy/lein-heroku"
   :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"})
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :release-tasks [["vcs" "assert-committed"]
+                  ["change" "version"
+                   "leiningen.release/bump-version" "release"]
+                  ["vcs" "commit"]
+                  ["deploy"]
+                  ["change" "version"
+                   "leiningen.release/bump-version"]
+                  ["vcs" "commit"]]
+  :vcs :git)
